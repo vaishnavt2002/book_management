8dae4df3982377277250f68a4877e0ad7c31da92
vaishnavt2002 <vaishnavt29@gmail.com>
2025-06-17 14:57:56 +0530

    fix: vercel config file and changes in vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  build: {
+    outDir: 'dist', 
+  },
+  base: '/', 
 })
